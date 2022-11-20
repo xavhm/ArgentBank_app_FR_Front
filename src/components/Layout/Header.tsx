3d13801491ed4be9e90729fbd5fe6ee1e60eaee9
Xavier Hernandez <xhernan.martinez@gmail.com>
2022-11-20 17:30:48 +0100

    fix - header styling

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.scss";
+import logo from "../../assets/argentBankLogo.png";
 
 const Header: React.FC = () => {
   return (
     <nav className={styles.main_nav}>
       <NavLink to="/" className={styles.main_nav_logo}>
-        <img
-          className={styles.main_nav_logo_image}
-          src="../../assets/argentBankLogo.png"
-          alt="Argent Bank Logo"
-        />
+        <img className={styles.main_nav_logo_image} src={logo} alt="Argent Bank Logo" />
         <h1 className={styles.sr_only}>Argent Bank</h1>
       </NavLink>
       <div>
