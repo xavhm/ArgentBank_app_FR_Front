edb9fbcab846bdb6153b00853d824ebf5ca3cfa5
Xavier Hernandez <xhernan.martinez@gmail.com>
2022-11-20 18:27:45 +0100

    add - Login page && NotFound page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,9 +3,27 @@ import styles from "./Login.module.scss";
 
 const Login: React.FunctionComponent = () => {
   return (
-    <div>
-      <p>Login</p>
-    </div>
+    <main className="main bg-dark">
+      <section className={styles.sign_in_content}>
+        <i className={`fa fa-user-circle ${styles.sign_in_icon}`}></i>
+        <h1>Sign In</h1>
+        <form>
+          <div className={styles.input_wrapper}>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className={styles.input_wrapper}>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className={styles.input_remember}>
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <button className={styles.sign_in_button}>Sign In</button>
+        </form>
+      </section>
+    </main>
   );
 };
 
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./NotFound.module.scss";
 
 const NotFound: React.FunctionComponent = () => {
   return (
-    <div>
-      <p>Error 404 : Page Not Found!</p>
-    </div>
+    <main className="main bg-dark">
+      <p className={styles.error}>Error 404 : Page was not found!</p>
+      <NavLink to="/" className={styles.redirection}>
+        Return to Homepage
+      </NavLink>
+    </main>
   );
 };
 
